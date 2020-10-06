41f8df22a71013a5ae056529df95118d2658cda1
kk-deng <71301799+kk-deng@users.noreply.github.com>
2020-10-05 23:41:35 -0400

    Update main.py
    Updated with def functions

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,20 +1,26 @@
 import os
 import csv
-'''
-def month_count(filepath):
-    return sum(1 for row in )
-'''
+
+def month_count(filename):
+    with open(filename, 'r') as csvfile:
+        csvreader = csv.reader(csvfile)
+        next(csvreader, None)
+        #Count the months/rows of the csv
+        return len(list(csvreader))
+
+def total_amount(filename):
+    with open(filename, 'r') as csvfile:
+        csvreader = csv.reader(csvfile)
+        next(csvreader, None)
+        all_amount = 0
+        for row in csvreader:
+            all_amount += int(row[1])
+        return all_amount
 
 # Path to collect data from the Resources folder
 pybank_csv = os.path.join("PyBank", "Resources", "budget_data.csv")
 
-with open(pybank_csv, 'r') as csvfile:
-    csvreader = csv.reader(csvfile)
-    next(csvreader, None)
-    #Count the months/rows of the csv
-    total_months = sum(1 for row in csvreader)
-    print(total_months)
-    print(csvreader)
-    total_amount = 0
-    for month in csvreader:
-        print(int(month[1]))
+print(month_count(pybank_csv))
+
+print(total_amount(pybank_csv))
+
