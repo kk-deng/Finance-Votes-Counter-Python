2af6ce84b630189697e29c0ad5de30c6a714d91b
kk-deng <71301799+kk-deng@users.noreply.github.com>
2020-10-10 22:10:53 -0400

    Update main.py
    Updated with pybank functions

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,26 +1,45 @@
 import os
 import csv
 
-def month_count(filename):
-    with open(filename, 'r') as csvfile:
-        csvreader = csv.reader(csvfile)
-        next(csvreader, None)
-        #Count the months/rows of the csv
-        return len(list(csvreader))
+def month_count(listname):
+    # Get the length of a list
+    return len(listname)
 
-def total_amount(filename):
-    with open(filename, 'r') as csvfile:
-        csvreader = csv.reader(csvfile)
-        next(csvreader, None)
-        all_amount = 0
-        for row in csvreader:
-            all_amount += int(row[1])
-        return all_amount
+def avg_change(listname):
+    # Get the mean by calculating sum/len
+    avg = sum(listname)/len(listname)
+    # Round to 2 decimals
+    return round(avg, 2)
+    
 
 # Path to collect data from the Resources folder
 pybank_csv = os.path.join("PyBank", "Resources", "budget_data.csv")
 
-print(month_count(pybank_csv))
+with open(pybank_csv, "r") as pb_file:
+    pb_csv = csv.reader(pb_file)
+    next(pb_csv, None)
 
-print(total_amount(pybank_csv))
+    # Get the total month of the list
+    month_list = []
+    amount_list = []
+    change_list = []
+    for row in pb_csv:
+        month_list.append(row[0])
+        amount_list.append(int(row[1]))
 
+    for i, amount in enumerate(amount_list):
+        if i > 0:
+            monthly_change = amount_list[i] - amount_list[i-1]
+            change_list.append(monthly_change)
+    
+    max_row = change_list.index(max(change_list))
+    min_row = change_list.index(min(change_list))
+    max_month = month_list[max_row + 1]
+    min_month = month_list[min_row + 1]
+    
+    #print(month_count(amount_list))
+    #print(amount_list)
+    print(sum(amount_list))
+    print(avg_change(change_list))
+    print(f"{max_month} ($ {max(change_list)} )")
+    print(f"{min_month} ($ {min(change_list)} )")
