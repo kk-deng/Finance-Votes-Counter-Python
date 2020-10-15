8ab61002937ff02df3403f45f34e7eebd5a0c825
kk-deng <71301799+kk-deng@users.noreply.github.com>
2020-10-15 18:30:38 -0400

    Update main.py
    Added write file function

--- PyBank/main.py
+++ PyBank/main.py
@@ -17,13 +17,21 @@ def print_screen():
     print(f"Total Months: {month_count(month_list)}")
     print(f"Total: ${sum(amount_list)}")
     print(f"Average Change: ${avg_change(change_list)}")
-    print(f"Greatest Increase in Profits: {max_month} (${max(change_list)})")
-    print(f"Greatest Decrease in Profits: {min_month} (${min(change_list)})")
+    print(f"Greatest Increase in Profits: {max_month} (${max_change})")
+    print(f"Greatest Decrease in Profits: {min_month} (${min_change})")
 
 def write_file():
-    with open("Output/output.txt", "w") as result_file:
-        result_file.write("Financial Analysis\n")
-        result_file.write("----------------------------\n")
+    with open("Analysis/Output.txt", "w") as result_file:
+        result_file.write("Financial Analysis")
+        result_file.write("----------------------------")
+        result_file.write(f"Total Months: {month_count(month_list)}")
+        result_file.write(f"Total: ${sum(amount_list)}")
+        result_file.write(f"Average Change: ${avg_change(change_list)}")
+        result_file.write(f"Greatest Increase in Profits: {max_month} (${max_change})")
+        result_file.write(f"Greatest Decrease in Profits: {min_month} (${min_change})")
+    
+    print("A result report was generated in " + result_file.name)
+
 
 
 # Path to collect data from the Resources folder
@@ -37,7 +45,7 @@ with open(pybank_csv, "r") as pb_file:
     # Get the total month of the list
     month_list = []
     amount_list = []
-    change_list = [0]
+    change_list = []
     for row in pb_csv:
         month_list.append(row[0])
         amount_list.append(int(row[1]))
@@ -47,13 +55,16 @@ with open(pybank_csv, "r") as pb_file:
             monthly_change = amount_list[i] - amount_list[i-1]
             change_list.append(monthly_change)
     
-    month_change_zipped = zip(month_list, change_list)
-
+    month_change_zipped = zip(month_list[1:], change_list)
+    max_change = max(change_list)
+    min_change = min(change_list)
+    
     for (a, b) in month_change_zipped:
         # If the monthly change equals to the max of the change list, get max month
-        if b == max(change_list):
+        if b == max_change:
             max_month = a
-        elif b == min(change_list):
+        elif b == min_change:
             min_month = a
     
     print_screen()
+    write_file()
