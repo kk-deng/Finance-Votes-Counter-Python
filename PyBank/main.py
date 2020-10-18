0ff5afd6a89fdcd308d5741b7f2835305e43a094
kk-deng <71301799+kk-deng@users.noreply.github.com>
2020-10-17 22:11:06 -0400

    Updated the output file name
    Finished all comments in main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,17 +1,18 @@
+# Import required libraries
 import os
 import csv
 
+# ------------- Definition of functions -------------
 def month_count(listname):
     # Get the length of a list
     return len(listname)
 
 def avg_change(listname):
-    # Get the mean by calculating sum/len
-    avg = sum(listname)/len(listname)
-    # Round to 2 decimals
-    return round(avg, 2)
+    # Get the mean by calculating sum/len, with round to 2 decimals
+    return round((sum(listname)/len(listname)), 2)
 
 def export_result():
+    # Assign the export result summary
     result = ("Financial Analysis\n"
         "----------------------------\n"
         f"Total Months: {month_count(month_list)}\n"
@@ -20,21 +21,26 @@ def export_result():
         f"Greatest Increase in Profits: {max_month} (${max_change})\n"
         f"Greatest Decrease in Profits: {min_month} (${min_change})")
     
-    print(result)
-    
-    txt_path = os.path.join("PyBank", "Analysis", "Output.txt")
+    # Write result to PB_Output.txt file
+    txt_path = os.path.join("PyBank", "Analysis", "PB_Output.txt")
     with open(txt_path, "w") as result_file:
         result_file.write(result)
     
-    print("----------------------------")
-    print("A result report was generated in " + result_file.name)
+    # Print out result to terminal
+    print(result)
+    print("----------------------------\n"
+        "A result report was generated in " + result_file.name
+        )
+
+# ------------- End of definition of functions -------------
 
 
-# Path to collect data from the Resources folder
+# Path of targeted csv in Resources folder
 pybank_csv = os.path.join("PyBank", "Resources", "budget_data.csv")
 
+# Main function to read csv file
 with open(pybank_csv, "r") as pb_file:
-
+    # Get csv and move to 2nd line
     pb_csv = csv.reader(pb_file)
     next(pb_csv, None)
 
@@ -45,7 +51,9 @@ with open(pybank_csv, "r") as pb_file:
 
     # Loop the csv file and add Months/Amount to the list
     for row in pb_csv:
+        # Append month name to the list
         month_list.append(row[0])
+        # Append integer amount to the list
         amount_list.append(int(row[1]))
 
     # Calculate the change of amount starting the 2nd month
@@ -62,11 +70,10 @@ with open(pybank_csv, "r") as pb_file:
 
     # Loop the zipped list, if the month has a value equals to max/min, record the months
     for (a, b) in month_change_zipped:
-        
         if b == max_change:
             max_month = a
         elif b == min_change:
             min_month = a
     
-    # Print and export result
-    export_result()
+# Print and export result
+export_result()
